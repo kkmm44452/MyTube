4606c3836a88b33bfad293cc403cb686870120ab
puneetdubey <kkmm44452@gmail.com>
2026-04-12 08:34:37 +0530

    fix secret exposet initialization

--- app/api/videos/route.ts
+++ app/api/videos/route.ts
@@ -10,6 +10,10 @@ export async function GET() {
 
     return Response.json(videos);
   } catch (error) {
+     
+  console.error("FULL ERROR:", error);
+  console.error("DB URL exists:", process.env.DATABASE_URL);
+
     console.error("VIDEOS API ERROR:", error);
 
     return new Response(
